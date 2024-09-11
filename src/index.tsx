e519b2727cf6d0c97e5d873d97ca19e09208a7d7
Foulest <43710301+Foulest@users.noreply.github.com>
2024-09-11 10:58:05 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';  // Import from react-dom/client for React 18
-import './index.css';
-import { App } from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const rootElement = document.getElementById('root');  // Get the root element
+import './index.css'
+import { App } from './App'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter } from 'react-router-dom'
+
+const rootElement = document.getElementById('root')
+
 if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);  // Use createRoot
+  const root = ReactDOM.createRoot(rootElement)
+
   root.render(
     <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </React.StrictMode>
-  );
+  )
 }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
