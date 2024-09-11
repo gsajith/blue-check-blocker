b6ca86275146726e3b63fe089c21f5cef71b7b75
Foulest <43710301+Foulest@users.noreply.github.com>
2024-09-11 13:11:10 -0700

    Commented out function

--- src/chrome/utils.ts
+++ src/chrome/utils.ts
@@ -1,12 +1,12 @@
-export const getCurrentTabUrl = (
-    callback: (url: string | undefined) => void
-): void => {
-    const queryInfo = {active: true, lastFocusedWindow: true}
-
-    chrome.tabs.query(queryInfo, (tabs) => {
-        callback(tabs[0].url)
-    })
-}
+// export const getCurrentTabUrl = (
+//     callback: (url: string | undefined) => void
+// ): void => {
+//     const queryInfo = {active: true, lastFocusedWindow: true}
+//
+//     chrome.tabs.query(queryInfo, (tabs) => {
+//         callback(tabs[0].url)
+//     })
+// }
 
 export const getCurrentTabUId = (
     callback: (id: number | undefined) => void
