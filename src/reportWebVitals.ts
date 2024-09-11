5d850ee4fc6c001e39af1567810b009bf62f9e17
Foulest <43710301+Foulest@users.noreply.github.com>
2024-09-11 13:51:17 -0700

    Update reportWebVitals.ts

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,14 @@
-import {type ReportHandler} from 'web-vitals'
+import {MetricType} from 'web-vitals'
 
-const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
-    if (onPerfEntry != null) {
-        import('web-vitals').then(({getCLS, getFID, getFCP, getLCP, getTTFB}) => {
-            getCLS(onPerfEntry)
-            getFID(onPerfEntry)
-            getFCP(onPerfEntry)
-            getLCP(onPerfEntry)
-            getTTFB(onPerfEntry)
-        })
+const reportWebVitals = (onPerfEntry?: (metric: MetricType) => void) => {
+    if (onPerfEntry) {
+        import("web-vitals").then(({onCLS, onINP, onFCP, onLCP, onTTFB}) => {
+            onCLS(onPerfEntry);
+            onINP(onPerfEntry);
+            onFCP(onPerfEntry);
+            onLCP(onPerfEntry);
+            onTTFB(onPerfEntry);
+        });
     }
 }
 
