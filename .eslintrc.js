f9597f0a1dc565a37b2a7e9c5746a7ae06537d28
Foulest <43710301+Foulest@users.noreply.github.com>
2024-09-11 10:55:56 -0700

    Fix React issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,16 @@ module.exports = {
     es2021: true
   },
   extends: ['plugin:react/recommended', 'standard-with-typescript'],
-  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['tsconfig.json']
   },
   plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect'  // Automatically detect the version of React
+    }
+  },
   rules: {}
 }
